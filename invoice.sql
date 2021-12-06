6ecbd874313616f9c5dd69f689b40076ea4e697a
Stephen Wall <sraywall@gmail.com>
2021-12-06 14:26:20 -0700

    finished invoice

---
+++ invoice.sql
@@ -0,0 +1,14 @@
+SELECT COUNT(*) FROM invoice WHERE billing_country = 'USA';
+SELECT MAX(total) FROM invoice;
+SELECT MIN(total) FROM invoice;
+SELECT COUNT(*) FROM invoice WHERE billing_state in ('CA','TX','AZ');
+SELECT AVG(total) FROM invoice;
+SELECT SUM(total) FROM invoice;
+
+UPDATE invoice
+SET total = 24
+WHERE invoice_id = 5;
+
+DELETE
+FROM invoice
+WHERE invoice_id = 1;
